9a217c5a0376338956690975926efbb7f0a2d0e4
Rennan Reis <rennanreis@me.com>
2025-06-03 08:57:16 -0300

    test(login): add successful login scenario

--- cypress/tests/e2e/login.spec.ts
+++ cypress/tests/e2e/login.spec.ts
@@ -1,10 +1,22 @@
 describe('Login com sucesso', () => {
   it('Deve fazer login com um usuário válido', () => {
-    // Implemente os passos do caso de teste aqui
+    // 1. Visite a página de login
+    cy.visit('/signin');
+
+    // 2. Preencha os campos com credenciais válidas
+    cy.get('[data-test="signin-username"]').type('Kylie.Padberg');
+    cy.get('[data-test="signin-password"]').type('s3cret');
+
+    // 3. Clique no botão de login
+    cy.get('[data-test="signin-submit"]').click();
+
+    // 4. Validações adicionais
+    cy.url({ timeout: 10000 }).should('include', '/');
+    cy.get('[data-test="sidenav-user-full-name"]').should('be.visible');
   });
 });
 
-describe('Tentar fazer login com credenciais inválidas', () => {
+/*describe('Tentar fazer login com credenciais inválidas', () => {
   it('Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas', () => {
     // Implemente os passos do caso de teste aqui
   });
@@ -20,4 +32,4 @@ describe('Tentar registrar um novo usuário com informações incompletas', () =
   it('Deve exibir mensagens de erro ao tentar registrar um novo usuário sem preencher todas as informações obrigatórias', () => {
     // Implemente os passos do caso de teste aqui
   });
-});
+});*/
