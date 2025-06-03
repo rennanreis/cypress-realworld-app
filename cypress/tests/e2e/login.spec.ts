df81cca6c9f1b8e3bff9166396348d787dc4c564
Rennan Reis <rennanreis@me.com>
2025-06-03 07:52:22 -0300

    feat(auth-tests): add initial structure for login and registration specs

---
+++ cypress/tests/e2e/login.spec.ts
@@ -0,0 +1,23 @@
+describe('Login com sucesso', () => {
+  it('Deve fazer login com um usuário válido', () => {
+    // Implemente os passos do caso de teste aqui
+  });
+});
+
+describe('Tentar fazer login com credenciais inválidas', () => {
+  it('Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas', () => {
+    // Implemente os passos do caso de teste aqui
+  });
+});
+
+describe('Registro de novo usuário com sucesso', () => {
+  it('Deve registrar um novo usuário com informações válidas', () => {
+    // Implemente os passos do caso de teste aqui
+  });
+});
+
+describe('Tentar registrar um novo usuário com informações incompletas', () => {
+  it('Deve exibir mensagens de erro ao tentar registrar um novo usuário sem preencher todas as informações obrigatórias', () => {
+    // Implemente os passos do caso de teste aqui
+  });
+});
